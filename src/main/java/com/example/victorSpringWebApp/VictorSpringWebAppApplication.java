27c136d0e5c50146ba767672817819d8e6f53757
Victor Ugo <mazi_viktor@Victors-Air.fios-router.home>
2021-01-29 19:08:20 -0500

    Spring Web App startup

---
+++ src/main/java/com/example/victorSpringWebApp/VictorSpringWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.victorSpringWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VictorSpringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VictorSpringWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/victorSpringWebApp/VictorSpringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.victorSpringWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VictorSpringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
